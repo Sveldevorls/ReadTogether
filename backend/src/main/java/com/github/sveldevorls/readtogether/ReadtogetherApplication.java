378d986a3cdb1f9b422fcb0eb3ab613dbf6c8999
Sveldevorls <64679979+Sveldevorls@users.noreply.github.com>
2025-11-17 01:33:41 +0800

    Set up project structure

---
+++ backend/src/main/java/com/github/sveldevorls/readtogether/ReadtogetherApplication.java
@@ -0,0 +1,13 @@
+package com.github.sveldevorls.readtogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadtogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadtogetherApplication.class, args);
+	}
+
+}
