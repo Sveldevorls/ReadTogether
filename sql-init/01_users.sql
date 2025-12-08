2110873cd06360cd854a3b84ed082e7c4ddb87e0
Sveldevorls <64679979+Sveldevorls@users.noreply.github.com>
2025-12-09 02:13:07 +0800

    Add default value in users table

--- sql-init/01_users.sql
+++ sql-init/01_users.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   -- Meta
   id INT NOT NULL AUTO_INCREMENT,
-  search_key VARCHAR(100) NOT NULL, -- String to compare against when searching, default value (no display name set) = username, replaced by display name once added
+  search_key VARCHAR(100) NOT NULL DEFAULT (`username`), -- String to compare against when searching, default value (no display name set) = username, replaced by display name once added
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   
