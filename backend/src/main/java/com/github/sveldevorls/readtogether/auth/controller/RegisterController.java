e62ca9ca62cb729baf41e5db22dcae217e536b7b
tmprvsr <243275881+tmprvsr@users.noreply.github.com>
2025-12-09 12:12:48 +0800

    Clean up auth controller import

--- backend/src/main/java/com/github/sveldevorls/readtogether/auth/controller/RegisterController.java
+++ backend/src/main/java/com/github/sveldevorls/readtogether/auth/controller/RegisterController.java
@@ -2,7 +2,6 @@ package com.github.sveldevorls.readtogether.auth.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
