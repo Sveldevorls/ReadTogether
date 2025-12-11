93621685ed95ff5ee9b33192ba1b66a885a486b9
Sveldevorls <64679979+Sveldevorls@users.noreply.github.com>
2025-12-12 02:15:10 +0800

    Add basic fetching logic in UserProfilePage

--- frontend/src/pages/UserProfilePage.vue
+++ frontend/src/pages/UserProfilePage.vue
@@ -1,14 +1,34 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router';
+import api from "@/util/api";
+import { ENDPOINTS } from "@/util/endpoints";
+import type { SuccessResponse, UserProfileResponse } from "@/util/responses";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
 
 const route = useRoute();
 const username = route.params.username;
+const fetchedProfile = ref<UserProfileResponse | null>(null);
+const isLoading = ref<boolean>(true);
+const isError = ref<boolean>(false);
+
+onMounted(async () => {
+  try {
+    const { data: response } = await api.get<SuccessResponse<UserProfileResponse>>(ENDPOINTS.USERS + "/" + username);
+    fetchedProfile.value = response.data;
+  } catch (error) {
+    isError.value = true;
+  } finally {
+    isLoading.value = false;
+  }
+});
 </script>
 
 <template>
-  User {{ username }}
+  <h1 v-if="isLoading">Loading...</h1>
+  <div v-else>
+    <h1 v-if="isError">404 NOT FOUND</h1>
+    <p v-else>{{ fetchedProfile }}</p>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
