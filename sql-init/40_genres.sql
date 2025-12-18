e4d17839c99ed103519a217b4fee438cf667c62b
Sveldevorls <64679979+Sveldevorls@users.noreply.github.com>
2025-12-19 02:05:40 +0800

    Add default genres

--- sql-init/40_genres.sql
+++ sql-init/40_genres.sql
@@ -13,3 +13,32 @@ CREATE TABLE genres (
     
     PRIMARY KEY (id)
 );
+
+-- Default sample genres
+INSERT INTO genres (genre_name, slug)
+VALUES
+    ("Art", "art"),
+    ("Biography", "biography"),
+    ("Business", "business"),
+    ("Comics", "comics"),
+    ("Cookbooks", "cookbooks"),
+    ("Crime", "crime"),
+    ("Fantasy", "fantasy"),
+    ("Fiction", "fiction"),
+    ("Graphic Novels", "graphic-novels"),
+    ("Historical Fiction", "historical-fiction"),
+    ("History", "history"),
+    ("Horror", "horror"),
+    ("Humor and Comedy", "humor-and-comedy"),
+    ("Manga", "manga"),
+    ("Music", "music"),
+    ("Mystery", "mystery"),
+    ("Nonfiction", "nonfiction"),
+    ("Poetry", "poetry"),
+    ("Psychology", "psychology"),
+    ("Romance", "romance"),
+    ("Science", "science"),
+    ("Science Fiction", "science-fiction"),
+    ("Sports", "sports"),
+    ("Thriller", "thriller"),
+    ("Travel", "travel");
